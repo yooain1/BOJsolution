d6ea8d10cdf37e4021318dc592860f98eedd9a8d
yooain1 <yooain1@gmail.com>
2016-10-30 01:58:49 +0900

    A-B

---
+++ BOJ1001.java
@@ -0,0 +1,19 @@
+/*
+  Baekjoon Online Judge
+
+  1001번 :: A-B
+  두 수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+*/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args)
+    {
+        int a,b;
+        Scanner keyboard = new Scanner(System.in);
+        a = keyboard.nextInt();
+        b = keyboard.nextInt();
+        System.out.println(a-b);
+    }
+}
